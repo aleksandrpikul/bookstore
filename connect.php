51d01230915c28787d6b5b4bf4cffd08705f6cf3
aleksandrpikul <96938106+aleksandrpikul@users.noreply.github.com>
2022-01-01 17:23:03 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-	$connection = mysqli_connect('localhost', 'root', '5199036', 'my-db2');
+	$connection = mysqli_connect('localhost', 'root', '', 'my-db2');
  mysqli_query($connection, "SET NAMES 'utf8'");
 	if(!$connection){
 		echo "Error: Unable to connect to MySQL." . PHP_EOL;
